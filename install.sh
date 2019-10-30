a2d16656f8e9b6ee8b47dffdd2d666f7663b8b4a
Daniel Onggunhao <dan@daniel.ai>
2019-10-30 23:54:39 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # Install script for Binance Chain
+# Note: this is based on current structure of `node-binary` repo, which is not optimal
+# Future improvement: version binaries using git, instead of folder structure
 
 # Detect operating system
-# TODO(Dan): Refactor into helper function
+# Future Improvement: Refactor into helper function
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   DETECTED_OS="linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -16,7 +18,7 @@ elif [[ "$OSTYPE" == "win32" ]]; then
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
   DETECTED_OS="linux"
 else
-  echo "Error: unable to detect operating system. Please install manually be referring to $DOCS_WEB_LINK"
+  echo "Error: unable to detect operating system. Please install manually by referring to $DOCS_WEB_LINK"
   exit 1
 fi
 
@@ -24,7 +26,7 @@ fi
 if [ ! -x /usr/bin/wget ]; then
   # some extra check if wget is not installed at the usual place
   command -v wget >/dev/null 2>&1 || {
-    echo >&2 "Error: you need to have wget installed and in your path"
+    echo >&2 "Error: you need to have wget installed and in your path. Use brew (mac) or apt (unix) to install wget"
     exit 1
   }
 fi
@@ -42,6 +44,7 @@ GH_RAW_PREFIX="raw/master"
 GH_REPO_DL_URL="$GH_REPO_URL/$GH_RAW_PREFIX"
 
 # Version selection options
+# Future improvement: pull dynamically from version list
 OPTION_VERSION_NUMBER=("0.5.8" "0.5.9" "0.5.10" "0.6.0" "0.6.1" "0.6.2")
 OPTION_NODE_TYPE=("Full Node" "Light Node")
 OPTION_NETWORK=("Mainnet" "Testnet")
@@ -84,30 +87,44 @@ select opt in "${OPTION_NETWORK[@]}"; do
 done
 
 # Download the selected binary
-SELECTED_BINARY_PATH="$NODE_TYPE/$NETWORK/$VERSION_NUMBER"
-DOWNLOAD_URL_PATH="$GH_REPO_URL/$GH_RAW_PREFIX/$SELECTED_BINARY_PATH/$DETECTED_OS"
+# Future improvement: versions should just be a single .zip payload (e.g. 0.6.2)
+# Future improvement: should not use folder structure as addressing method
+VERSION_PATH="$NODE_TYPE/$NETWORK/$VERSION_NUMBER"
+GH_BASE_URL="$GH_REPO_URL/$GH_RAW_PREFIX/$VERSION_PATH"
+CONFIG_DOWNLOAD_URL="$GH_BASE_URL/config"
+NODE_BINARY_DOWNLOAD_URL="$GH_BASE_URL/$DETECTED_OS"
 
-echo ${DOWNLOAD_URL_PATH}
+mkdir temp-bnbchain-installer
+cd temp-bnbchain-installer
 
 # wget the binary, config files
-wget ${DOWNLOAD_URL_PATH}
+# Future improvement: should refactor in the future with releases in a single .zip or .tar.gz file
+if [ $NODE_TYPE == "fullnode" ]; then
+  # Future improvement: should be refactored into helper function
 
-# Create .bnbchaind in root
-# echo "... creating $BNC_HOME_DIR"
-# if [ -d "$BNC_HOME_DIR" ]; then
-#   echo "Error: Binance Chain client has already been installed. Please remove contents of ${BNC_HOME_DIR} before reinstalling."
-#   exit 1
-# else
-#   mkdir -p $BNC_HOME_CONFIG_DIR
-# fi
+  # Create .bnbchaind in root
+  echo "... creating $BNC_HOME_DIR"
+  if [ -d "$BNC_HOME_DIR" ]; then
+    echo "Error: Binance Chain client has already been installed. Please remove contents of ${BNC_HOME_DIR} before reinstalling."
+    exit 1
+  else
+    mkdir -p $BNC_HOME_CONFIG_DIR
+  fi
 
-# # Copy config files over to ~/.bnbchaind
-# echo "... copying version-specfic files to $BNC_HOME_DIR"
-# for i in `ls $SELECTED_BINARY_PATH`
-# do
-#   echo "    $i"
-#   cp "$SELECTED_BINARY_PATH/$i" $BNC_HOME_CONFIG_DIR
-# done
+  wget "$CONFIG_DOWNLOAD_URL/app.toml"
+  wget "$CONFIG_DOWNLOAD_URL/config.toml"
+  wget "$CONFIG_DOWNLOAD_URL/genesis.json"
+  wget "$NODE_BINARY_DOWNLOAD_URL/bnbchaind"
+
+  # Copy config files over to ~/.bnbchaind
+  echo "... copying version-specfic files to $BNC_HOME_DIR"
+  for i in $(ls $pwd); do
+    echo "    $i"
+    cp "$pwd/$i" $BNC_HOME_CONFIG_DIR
+  done
+elif [ $NODE_TYPE == "lightnode" ]; then
+  echo "$NODE_BINARY_DOWNLOAD_URL/lightd"
+fi
 
 # # Connect to seed
 # # TODO(Dan): Find more elegant way to load variables from config.toml
@@ -115,3 +132,6 @@ wget ${DOWNLOAD_URL_PATH}
 # echo "$seeds"
 
 # # Add installed version of Binance Chain to path
+echo "======= Installation complete ======="
+echo "Please add bnbchaind to your path"
+echo "  export PATH=\$PATH:/usr/local/.bnbchaind/bin"
